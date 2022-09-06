d12215227b2cc766bb1a0124093bafe47b747682
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-09-06 18:12:06 +0530

    Add files via upload

---
+++ Common_Errors.py
@@ -0,0 +1,11 @@
+# print(44/0)  # ZeroDivisionError
+
+# print(lok)  #NameError
+
+# h="lokesh"
+# print(int(h))  #ValueError
+
+# b=5
+# print(b+"king")  #TypeError
+# b=5.6
+# print(b+"king")  #TypeError
---
+++ Error_Handling.py
@@ -0,0 +1,212 @@
+""" Part-1 """
+# print("Python")
+# print("Core Python")
+# try:
+#     print(9/0)
+# except:
+#     print("You Have Divided by Zero")
+# print("Lokesh")
+# print("Developer")
+
+""" part-2 """
+# print("welocme to 2nd part")
+# print("of Python")
+# try:
+#     print(15/3)
+# except:
+#     print("Not ok Divided by Zero")
+# print("learing Error Hnadling")
+# print("2nd part ends")
+
+""" Part-3 """
+# print("Welcome to 3rd part")
+# print("Core python")
+# a=5
+# try:
+#     print(c)
+# except ZeroDivisionError:
+#     print("Not ok Divided by Zero")
+# print("Lokesh")
+# print("3rd part ends")
+
+#example:-
+
+# a=55
+# try:
+#     print(b)
+# except NameError:
+#     print("Hi")
+
+""" Part-4 """
+# print("Welocome to 4th part")
+# print("Core Python")
+# a=55
+# try:
+#     print(a/5)
+#     print(b)
+# except ZeroDivisionError:
+#     print("Not Ok Divided by Zero")
+# print("Lokesh")
+# print("part 4 ends")
+
+""" Part-5 """
+# print("Welcome to 5th part")
+# print("Hello Core Python")
+# a=6
+# try:
+#     print(a/3)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("Not ok divided by zero")
+# print("Hello,Lokesh")
+# print("5th part ends")
+
+""" Part-6 """
+# print("6th part statrts")
+# print("in Core Python")
+# b=4
+# try:
+#     print(b/0)
+#     print(6)
+# except ZeroDivisionError:
+#     print("Not ok divided by zero")
+# except NameError:
+#     print("You have not defined the name")
+# print("Lokesh")
+# print("6th part ends")
+
+""" Part-7 """
+# print("7th part starts")
+# a=6
+# b="lokesh"
+# try:
+#     print(a/2)
+#     print(a)
+#     print(a/0)
+# except:
+#     print("Not ok divided by zero")
+# print("Hello pep-8")
+# print("7th part ends")
+
+""" Part-8 """
+# print("Welcome to 8th part")
+# print("Core python")
+# a=3
+# b='8'
+# try:
+#     print(a/3)
+#     print(a/0)
+#     print("lokesh")
+# except ZeroDivisionError:
+#     print("Hello IT")
+# print("Hi world")
+# print("erororor")
+
+""" Part-9 """
+# print("9th Part sstarts")
+# print("Core Python")
+# a=4
+# c='55'
+# try:
+#     print(a/2)
+#     print(a/0)
+#     print(a+a)
+# except ZeroDivisionError:
+#     print("Advance Pythom")
+# print("Python Analytics")
+
+""" Part-10 """
+# print("Core Python")
+# print("Advance Python")
+# a=9
+# b='55'
+# try:
+#     print(a/3)
+#     print(a/0)
+#     print(b+'love')
+# except ZeroDivisionError:
+#     print("hello hyd")
+# print("got it")
+
+""" Part-11 """
+# print("lokesh")
+# print("kong fu pands")
+# a=8
+# c='88'
+# try:
+#     print(a/4)
+#     print(a/0)
+#     # print(%)
+# except SyntaxError:
+#     print("king kohli")
+# print("rohitman")
+# print("dravid")
+
+""" Part-12 """
+# print("harkins")
+# print("Guido van rossum")
+# a=5
+# b='41'
+# try:
+#     print(a/5)
+# except ZeroDivisionError:
+#     print("hi lokesh")
+# finally:
+#     print("jai sri ")
+# print("bhuvi")
+# print("ravi bish")
+
+""" Part-13 """
+# print("kiwi")
+# print("hell with the rules")
+# a=6
+# b='645'
+# try:
+#     print(a/6)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("divided by zero")
+# finally:
+#     print("print this also")
+# print("josh innovation")
+# print("print it")
+
+""" Part-14 """
+# print("jamadala")
+# print("manyam")
+# a=7
+# b='24'
+# try:
+#     print(a/7)
+#     print(a/14)
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# else:
+#     print("its ok for now don't repeat it again")
+# print("joy")
+# print('avesg')
+
+""" Part-15"""
+# print("jamadala")
+# print("manyam")
+# a=7
+# b='24'
+# try:
+#     print(a/7)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# else:
+#     print("its ok for now don't repeat it again")
+# print("joy")
+# print('avesg')
+
+""" Assertion Error """
+# n=int(input("Enter the value of n:"))
+# if n<21:
+#     assert print("You are not elligible for living relationship")
+
+""" Making your own errors with exception """
+# b=int(input("Enter the value more than 18:"))
+# if (b>18):
+#     raise Exception("you are elligible to vote")
